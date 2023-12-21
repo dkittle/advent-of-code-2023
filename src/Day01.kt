e9006069b18baecb679564f902a448f0a2193c44
Don Kittle <don@kittle.ca>
2023-12-21 09:54:10 -0500

    Solution for Day01, part 2 Phew, got to use that recursive function after all 😅

--- src/Day01.kt
+++ src/Day01.kt
@@ -4,7 +4,48 @@ private val START_SEARCH = Pair(NOT_FOUND, NOT_FOUND)
 
 fun Pair<Int, Int>.fixFindings() = if (second == NOT_FOUND) Pair(first, first) else this
 
+val numberWords = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9
+)
+
+val minimumWordLength = numberWords.keys.map { it.length }.min()
+
 fun main() {
+
+    fun parseForNumberOrWord(line: String): Int? {
+        val number = if (line.isNotEmpty()) {
+            line.take(1).toIntOrNull() ?: run {
+                val foundWord = numberWords.keys.firstOrNull { line.startsWith(it) }
+                foundWord?.let {
+                    numberWords.get(it)
+                }
+            }
+        } else null
+        return number
+    }
+
+    tailrec fun parseLineForBookendNumbersOrWords(
+        line: String,
+        findings: Pair<Int, Int> = START_SEARCH
+    ): Pair<Int, Int> {
+        val maybeNumber = parseForNumberOrWord(line)
+        val updatedFindings = maybeNumber?.let {
+            if (findings.first == NOT_FOUND)
+                Pair(it, findings.second)
+            else
+                Pair(findings.first, it)
+        } ?: findings
+        return if (line.length <= 1) updatedFindings.fixFindings()
+        else parseLineForBookendNumbersOrWords(line.drop(1), updatedFindings)
+    }
 
     tailrec fun parseLineForBookendNumbers(line: String, findings: Pair<Int, Int> = START_SEARCH): Pair<Int, Int> {
         val maybeNumber = if (line.isNotEmpty()) {
@@ -16,7 +57,10 @@ fun main() {
             else
                 Pair(findings.first, it)
         } ?: findings
-        return if (line.length == 1) updatedFindings.fixFindings() else parseLineForBookendNumbers(line.drop(1), updatedFindings)
+        return if (line.length <= 1) updatedFindings.fixFindings() else parseLineForBookendNumbers(
+            line.drop(1),
+            updatedFindings
+        )
     }
 
     fun findBookendNumbers(line: String): Pair<Int, Int> {
@@ -27,7 +71,6 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         return input.map {
-//            parseLineForBookendNumbers(it)
             findBookendNumbers(it)
         }.map { (first, second) ->
             "$first$second"
@@ -39,7 +82,15 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map {
+            parseLineForBookendNumbersOrWords(it)
+        }.map { (first, second) ->
+            "$first$second"
+        }.mapNotNull {
+            it.toIntOrNull()
+        }.sumOf {
+            it
+        }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -48,5 +99,10 @@ fun main() {
 
     val input = readInput("Day01")
     check(part1(input) == 53921)
+
+    val testInput2 = readInput("Day01_test2")
+    println("${part2(testInput2)}")
+    check(part2(testInput2) == 281)
+
     part2(input).println()
 }
