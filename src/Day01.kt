33bfc8d35719d8bec1c51633bbf35f22708a0c31
Don Kittle <don@kittle.ca>
2023-12-21 08:46:53 -0500

    Solution for Day01, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,41 @@
+const val NOT_FOUND = -1
+
+private val START_SEARCH = Pair(NOT_FOUND, NOT_FOUND)
+
+fun Pair<Int, Int>.fixFindings() = if (second == NOT_FOUND) Pair(first, first) else this
+
 fun main() {
+
+    tailrec fun parseLineForBookendNumbers(line: String, findings: Pair<Int, Int> = START_SEARCH): Pair<Int, Int> {
+        val maybeNumber = if (line.isNotEmpty()) {
+            line.take(1).toIntOrNull()
+        } else null
+        val updatedFindings = maybeNumber?.let {
+            if (findings.first == NOT_FOUND)
+                Pair(it, findings.second)
+            else
+                Pair(findings.first, it)
+        } ?: findings
+        return if (line.length == 1) updatedFindings.fixFindings() else parseLineForBookendNumbers(line.drop(1), updatedFindings)
+    }
+
+    fun findBookendNumbers(line: String): Pair<Int, Int> {
+        val initial = line.first { it.isDigit() }
+        val final = line.last { it.isDigit() }
+        return Pair(initial.digitToInt(), final.digitToInt())
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map {
+//            parseLineForBookendNumbers(it)
+            findBookendNumbers(it)
+        }.map { (first, second) ->
+            "$first$second"
+        }.mapNotNull {
+            it.toIntOrNull()
+        }.sumOf {
+            it
+        }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +44,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
-    part1(input).println()
+    check(part1(input) == 53921)
     part2(input).println()
 }
